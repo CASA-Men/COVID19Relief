48282629786259d181dfa80b6afd39713a40b779
Chika N <natechizzy@users.noreply.github.com>
2020-04-12 18:47:24 +0100

    addition of login and forgot password views

---
+++ COVID19Ninjas/ViewModels/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVID19Ninjas.ViewModels
+{
+    public class AccountViewModel
+    {
+    }
+
+    public class LoginViewModel
+    {
+        
+    }
+}
